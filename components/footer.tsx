63564bc139ea9d6d52c2f3115099c79b7b1062d9
smit <ajanismit5@gmail.com>
2025-01-16 16:24:42 +0530

    initial commit

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return(
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-xs text-center text-black">
+                    &copy; 2024 Big Bazaar Store. All rights reserved.
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ components/ui/currency.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+export const formatter = new Intl.NumberFormat("en-US",{
+    style:'currency',
+    currency:'USD'
+  })
+
+interface CurrencyProps {
+    value?: string | number;
+}
+const Currency : React.FC<CurrencyProps> = ({
+    value
+}) => {
+    const [isMounted,setIsMounted] = useState(false);
+
+    useEffect(()=>{
+        setIsMounted(true)
+    },[])
+
+    if(!isMounted) return null
+    
+    return (
+        <span className="font-semibold">
+            {formatter.format(Number(value))}
+        </span>
+    )
+}
+
+export default Currency;
